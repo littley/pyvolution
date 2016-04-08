4de55793840618d9a3892e3e793863d6058dad6f
littley <cody@littley.us>
2016-04-08 11:15:35 -0500

    added some new gene types

--- GeneLibrary.py
+++ GeneLibrary.py
@@ -13,7 +13,7 @@ def FloatGeneType(description,
                   mutationSTDEV=1,
                   mutatorGene=None):
     """
-    Regurn a GeneType configured as specified
+    Regurn a GeneType of type float configured as specified
     :param description: the description/name of this gene
     :type description: str
     :param minVal: the minimum value allowed for this gene.  Values that end up smaller will be rounded to the minVal
@@ -21,11 +21,17 @@ def FloatGeneType(description,
     :type minVal: float
     :param maxVal: the maximum value allowed for this gene.  Values that end up larger will be rounded to the maxVal
                     A value of None means that there is no maxVal
+    :type maxVal: float
     :param generatorAverage: the average value that is generated for new genes
                     A value of None will cause a flat distribution to be used
+    :type generatorAverage: float
     :param generatorSTDEV: the standard devation of the values generated for new genes
-    :param averageMutation: the average amount that this gene changes during each mutation
-    :param mutationSTDEV:
+    :type generatorSTDEV: float
+    :param averageMutation: the average amount that this gene changes during each mutation.  You should have a VERY
+                                good reason to set this to something other than the default value
+    :type averageMutation: float
+    :param mutationSTDEV: The standard deviation of the change during mutation.
+    :type mutationSTDEV: float
     :param mutatorGene: the description of a gene.  If not None, the value of this gene will be used instead of
                             the mutationSTDEV.  If it is None then it is ignored
     :type mutatorGene: str
@@ -49,17 +55,105 @@ def FloatGeneType(description,
             return result
 
     def mutator(originalValue, chromosome):
-
         stdev = mutationSTDEV
-
         if mutatorGene is not None:
             stdev = chromosome[mutatorGene]
-
         result = originalValue + random.normalvariate(averageMutation, stdev)
         result = max(min(result, maxVal), minVal)
         return result
 
     return GeneType(generator, mutator, description)
+
+##################################################################################################
+
+def IntGeneType(description,
+                minVal=None,
+                maxVal=None,
+                generatorAverage=None,
+                generatorSTDEV=1,
+                averageMutation=0,
+                mutationSTDEV=1,
+                mutatorGene=None):
+    """
+    Regurn a GeneType of type int configured as specified
+    :param description: the description/name of this gene
+    :type description: str
+    :param minVal: the minimum value allowed for this gene.  Values that end up smaller will be rounded to the minVal
+                    A value of None means that there is no minVal
+    :type minVal: int
+    :param maxVal: the maximum value allowed for this gene.  Values that end up larger will be rounded to the maxVal
+                    A value of None means that there is no maxVal
+    :type maxVal: int
+    :param generatorAverage: the average value that is generated for new genes
+                    A value of None will cause a flat distribution to be used
+    :type generatorAverage: float
+    :param generatorSTDEV: the standard devation of the values generated for new genes
+    :type generatorSTDEV: float
+    :param averageMutation: the average amount that this gene changes during each mutation.  You should have a VERY
+                                good reason to set this to something other than the default value
+    :type averageMutation: float
+    :param mutationSTDEV: The standard deviation of the change during mutation.
+    :type mutationSTDEV: float
+    :param mutatorGene: the description of a gene.  If not None, the value of this gene will be used instead of
+                            the mutationSTDEV.  If it is None then it is ignored
+    :type mutatorGene: str
+    :rtype: GeneType
+    """
+
+    if minVal is None:
+        minVal = -1 * sys.maxint
+    if maxVal is None:
+        maxVal = sys.maxint
+
+    def generator():
+        if generatorAverage is None:
+            return int(round(random.uniform(minVal, maxVal)))
+        else:
+            result = random.normalvariate(generatorAverage, generatorSTDEV)
+            if maxVal is not None:
+                result = min(result, maxVal)
+            if minVal is not None:
+                result = max(result, minVal)
+            return int(round(result))
+
+    def mutator(originalValue, chromosome):
+        stdev = mutationSTDEV
+        if mutatorGene is not None:
+            stdev = chromosome[mutatorGene]
+        result = originalValue + random.normalvariate(averageMutation, stdev)
+        result = max(min(result, maxVal), minVal)
+        return int(round(result))
+
+    return GeneType(generator, mutator, description)
+
+##################################################################################################
+
+def BoolGeneType(description,
+                 probabilityTrue=0.5,
+                 mutationProbability=1.0):
+    """
+    Return a gene of type bool configured as specified
+    :param description: the description/name of the gene
+    :type description: str
+    :param probabilityTrue: The probability that a randomly generated gene is true
+    :type probabilityTrue: float
+    :param mutationProbability: The probability that this gene will "flip" when mutated
+    :type mutationProbability: float
+    :rtype: GeneType
+    """
+
+    def generator():
+        return random.uniform(0.0, 1.0) < probabilityTrue
+
+    def mutator(originalValue, chromosome):
+        if random.uniform(0.0, 1.0) <= mutationProbability:
+            return not originalValue
+        else:
+            return originalValue
+
+    return GeneType(generator, mutator, description)
+
+##################################################################################################
 
 def FloatInverseFit(description, maxVal=1, startVal=1):
     """
@@ -83,6 +177,3 @@ def FloatInverseFit(description, maxVal=1, startVal=1):
         return originalValue
 
     return GeneType(generator, mutator, description)
-
-
-
---
+++ examples/README.txt
@@ -0,0 +1,6 @@
+Some examples of the use of pyvolution are provided here.
+
+To run them, use "python name_of_example_file.py"
+
+A symolic link to pyvolution is included in this directory.  This is to enable you to run the examples files
+without fully installing pyvolution.
